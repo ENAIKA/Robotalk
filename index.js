952cfd72de8f7cd65d2e1131f73a606e5e163cb2
NgugiMuthoni <ngugimuthoni43@gmail.com>
2020-01-30 15:52:23 +0300

    files joined

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-function myFunction() {
-    var input, filter, ul, li, a, b, txtValue;
-    input = document.getElementById("search");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("list");
-    li = ul.getElementsByTagName("li");
-    for (b = 0; b < li.length; b++) {
-        a = li[b].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[b].style.display = "";
-        } else {
-            li[b].style.display = "none";
-        }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function myFunction() {
+     var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName("li");
+      for (i = 0; i < li.length; i++) {
+          a = li[i].getElementsByTagName("a")[0];
+          txtValue = a.textContent || a.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              li[i].style.display = "";
+          } else {
+              li[i].style.display = "none";
+          }
+       }
     }
-}
+  
